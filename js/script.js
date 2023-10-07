e97c8dd23441ac25c38923ee71756c0120956e61
Rivaldi Syaputra <rivaldi191@gmail.com>
2023-10-07 15:13:54 +0700

    Update navbar, card menu, add map pada contact

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-$(document).ready(function() {
-    $('.effect').click(function() {
-        $('.effect').removeClass('active');
-        $(this).addClass('active');
-    });
+$(document).ready(function () {
+  $(".effect").click(function () {
+    $(".effect").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  //  Content Menu Hover
+  $(".cardKonten").hover(
+    function () {
+      $(this).addClass("hovered"); // Menambahkan class 'hovered' saat card di-hover
+    },
+    function () {
+      $(this).removeClass("hovered"); // Menghapus class 'hovered' saat card tidak di-hover
+    }
+  );
 });
